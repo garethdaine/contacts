4d276970030d022ec6a4cce54bf2673c0cc9ab30
Gareth Daine <garethdaine@googlemail.com>
2023-07-18 12:16:17 +0100

    Added Button.vue and modified readme.md.

---
+++ Button.vue
@@ -0,0 +1,71 @@
+<!-- Moved template to top of file as point of personal preference -->
+<template>
+    <button :class="classes" @click="props.to && navigateTo(props.to)" :disabled="props.disabled || props.loading">
+        <slot name="before">
+            <app-icon v-if="props.iconLeft" :icon="props.iconLeft" />
+        </slot>
+        <span>
+            <slot v-if="!props.loading"></slot>
+            <slot v-if="props.loading" name="loading"></slot>
+        </span>
+        <app-loading v-if="props.loading" size="sm" class="inline-block ml-1" />
+        <slot name="after">
+            <app-icon v-if="props.iconRight" :icon="props.iconRight" :lg="props.iconLarge" />
+        </slot>
+    </button>
+</template>
+
+<!-- Added TypeScript support and some indentation as part of personal preference. -->
+<script setup lang="ts">
+// Imported required
+import { ref, computed } from 'vue'
+// Assuming `navigateTo` is a function, import it appropriately.
+
+// Fixed typo when using the `defineProps` API/compiler macro.
+const props = defineProps({
+    classes: {
+        type: Array,
+        default: () => []
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    iconLeft: String,
+    iconRight: String, // Defined iconRight.
+    iconLarge: {
+        type: Boolean,
+        default: false
+    },
+    loading: {
+        type: Boolean,
+        default: false
+    },
+    padding: String, // Added definition/configuration object for padding.
+    sm: Boolean,
+    text: Boolean,
+    to: String, // Added definition/configuration object for 'to'.
+    variant: {
+        type: String,
+        default: 'primary'
+    },
+})
+
+const buttonType = ref(props.text ? 'btn-text' : 'btn')
+
+const classes = computed(() => {
+    let classes = [...props.classes] // Make a copy of props.classes instead of modifying it directly, also, using wrong context with 'this' in Composition API.
+
+    classes.push(buttonType.value)
+
+    if (buttonType.value === 'btn') {
+        classes.push('w-full')
+    }
+
+    if (props.variant) {
+        classes.push(buttonType.value + '--' + props.variant)
+    }
+
+    return classes
+})
+</script>
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Contacts Test Application
+
+This is a test application which creates, retrieves, and searches  contacts database.
+
+It is build with Laravel and Inertia, using Laravel Jetstream.
+
+## Setup Steps
+
+1. Clone the repository
+
+```git clone https://github.com/garethdaine/contacts.git```
+
+2. Install composer dependencies
+
+```composer install```
+
+3. Build app
+
+```npm run build```
+
+4. Migrate the database
+
+```php artisan migrate```
+
+5. Serve the app (make sure Dockr is installed and setup)
+
+```./vendor/bin/sail up```
